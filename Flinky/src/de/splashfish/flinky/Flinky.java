ad92b2301eeb6c5fd024ed72f785f746462135e3
lukasdietrich <lukas@splashfish.de>
2012-07-05 23:59:34 +0200

    update

--- Flinky/src/de/splashfish/flinky/Flinky.java
+++ Flinky/src/de/splashfish/flinky/Flinky.java
@@ -10,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Flinky extends JavaPlugin {
 	
-	public 	static 	String				version		= "2.11";
+	public 	static 	String				version		= "2.12";
 	
 	private static	Logger 				pstream;
 	private	static	ConfigHandler		ch;
